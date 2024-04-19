727d9d6b6ca94b411a90014538943129a3a002e9
ETZ-RAHUL <132138127+ETZ-RAHUL@users.noreply.github.com>
2024-04-19 20:49:49 +0530

    Update config.py
    Signed-off-by: ETZ-RAHUL <132138127+ETZ-RAHUL@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/3a8be19846685da261f99.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/96af4f58a0676e9ff8a7b.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
