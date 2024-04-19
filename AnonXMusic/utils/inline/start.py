69d7fc6c98c8f905e587a284466ffa827ad280f2
ETZ-RAHUL <132138127+ETZ-RAHUL@users.noreply.github.com>
2024-04-19 21:40:53 +0530

    Update start.py
    Signed-off-by: ETZ-RAHUL <132138127+ETZ-RAHUL@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -28,6 +28,6 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-    
+        ],
     ]
     return buttons
